8342440afbc330de02a1726cd3327cf879225545
Samuel Angulo <samuelangulo70@gmail.com>
2024-10-27 02:53:07 +0100

    pasitoapasito3

--- src/pages/products/Product.jsx
+++ src/pages/products/Product.jsx
@@ -38,7 +38,7 @@ function Product(props) {
         </Link>
         <div className="card-body">
           <h5 className="card-title text-center text-dark text-truncate">
-            Muestra
+            hola
           </h5>
           <p className="card-text text-center text-muted mb-0">{offPrice}</p>
           <div className="d-grid d-block">
